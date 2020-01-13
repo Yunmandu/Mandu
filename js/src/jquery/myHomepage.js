793cff1239665425445d3730ca14d175c891cd8d
yunsoyeon <lll4010@gmail.com>
2020-01-13 20:35:55 +0900

    myhomepage

---
+++ js/src/jquery/myHomepage.js
@@ -0,0 +1,14 @@
+(function($){
+
+const wrap2 = $('#wrap_2');
+const headBox = wrap2.children('#headBox');
+const indicator = headBox.children('.indicator');
+const indi = indicator.find('.indi');
+const indiClick = indi.find('.click');
+const indiA = indiClick.find('a');
+
+
+ 
+
+
+})(jQuery);
